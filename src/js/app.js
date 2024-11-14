ac0e1b68e7b832afa7efad0bc85e4e4f3b169a64
Axel Lanuza <rojasf@ae.com>
2024-11-14 17:34:34 -0600

    1. First commit: Add basic html and styles

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+import { AccessibleFormUI } from "./test-module";
+import "../css/app.css";
+import "../scss/style.scss";
+
+/********** Paste your code here! ************/
+
+window.onload = () => {
+  console.log("Paste your code here!");
+  AccessibleFormUI.init();
+};
---
+++ src/js/test-module.js
@@ -0,0 +1,19 @@
+export const AccessibleFormUI = (() => {
+  console.log("AccessibleFormUI");
+
+  const init = () => {
+    console.log("Init AccessibleFormUI");
+    eventHandlers();
+  };
+
+  const renderForm = (e) => {
+    console.log("Render Form");
+  };
+
+  const eventHandlers = () => {
+  };
+
+  return {
+    init,
+  };
+})();
